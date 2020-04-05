4558aa4388663c1906f79318ae81f67f531e775f
Dabid <wti123@gmail.com>
2020-04-05 12:01:09 -0300

    01-2-empresa.js Challenge

---
+++ exercicios01/03.js
@@ -0,0 +1,14 @@
+const Usuario = {
+    Nome: "David",
+    Empresa: "Rocketseat",
+    Cor: "Roxo",
+    Foco:"Programação",
+    Endereço: {
+        Rua: "Rua Guilherme Gembala",
+        Numero: 260,
+    } 
+};
+
+console.log(` A Empresa ${Usuario.Empresa}  está localizada em ${Usuario.Endereço.Rua}, ${Usuario.Endereço.Numero}`)
+
+
