663e4089810407f2bfb22605822a36ecd17b5f53
Deeee <wtih123@gmail.com>
2020-06-18 20:33:38 -0300

    Modal finished :)

--- FrontEnd/conteudo/index.js
+++ FrontEnd/conteudo/index.js
@@ -3,10 +3,14 @@ const cards = document.querySelectorAll(".card")
 
 for (let card of cards) {
     card.addEventListener("click", function() {
+        const cursoId = card.getAttribute("id")
         modalOverlay.classList.add("active")
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${cursoId}`
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function() {
     modalOverlay.classList.remove("active")
 })
+
+modalOverlay.querySelector("iframe").src = "https://rocketseat.com.br/starter"
