9b3b0f3dd241f942a05c15d2ef06b9a6bdaa368c
Deeee <wtih123@gmail.com>
2020-06-20 12:58:37 -0300

    data courses

---
+++ data.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        id: "starter",
+        title: "Curso: starte",
+        duration: "Módulos: 8",
+        price: "Free"
+    },
+    {
+        id: "launchbase",
+        title: "Curso: LaunchBase",
+        duration: "Módulos: 8",
+        price: "Reservar Vaga" 
+    },
+    {
+        id: "gostack",
+        title: "Curso: Gostack",
+        duration: "Módulos: 10",
+        price: "Reservar Vaga"
+    },
+
+]
